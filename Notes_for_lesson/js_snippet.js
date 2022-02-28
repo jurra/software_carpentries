1d3f13cfe5e95a1f25acb285f1220731b92bcb19
jurra <jose.urra86@gmail.com>
2022-02-28 09:29:22 +0100

    updated files and folders with templates

---
+++ Notes_for_lesson/js_snippet.js
@@ -0,0 +1,7 @@
+var length = 0; var names = ['Curie', 'Darwin', 'Turing'];
+for (var n in names) { 
+    if (n == 'Curie') {console.log('Curie is a lady');
+    } else {console.log(n + ' is not a lady');}
+}
+
+var length = 0; var names = ['Curie', 'Darwin', 'Turing']; for (var n in names) { if (n == 'Curie') {console.log('Curie is a lady');} else {console.log(n + ' is not a lady');}}
